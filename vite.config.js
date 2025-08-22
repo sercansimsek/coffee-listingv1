1d240adfe6ed82304ae0dfd355ceea5fc0ae6ac5
Sercan <sercansimsek44@gmail.com>
2025-08-22 14:56:45 +0200

    cleanup 0.1

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: "/coffee-listingv1",
+	base: "/coffee-listingv1/",
 });
